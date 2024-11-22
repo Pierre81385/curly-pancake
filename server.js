7def312865f1fcd651b71c9cd3b3ebe8a879f9a4
Peter Bishop <pjb.den@gmail.com>
2024-11-22 13:18:51 -0700

    minor mistake fixed in server env

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
-const app = express();
-const PORT = process.env.PORT;
 
 dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Curly Pancake Server!');
   });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 console.log(`Server listening on http://localhost:${PORT}`);
 });
